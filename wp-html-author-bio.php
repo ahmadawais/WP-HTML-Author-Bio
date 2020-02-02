b3c7226d315080674be094fa9ec9ea082dcfd006
Ahmad Awais <me@AhmadAwais.com>
2020-02-03 03:14:32 +0500

    🐛 FIX: Lingo & Links

--- wp-html-author-bio.php
+++ wp-html-author-bio.php
@@ -3,9 +3,9 @@
  * Plugin Name: WP HTML Author Bio
  * Plugin URI: https://github.com/ahmadawais/WP-HTML-Author-Bio
  * Description: Allows you to use HTML and rel= in author's bio, this helps you to get indexed as an author in Google Search.
- * Author: WPCouple(Ahmad Awais & Maedah Batool)
- * Author URI: https://WPCouple.com/
- * Version: 1.1.2
+ * Author: TheDevCouple(Awais & Maedah)
+ * Author URI: https://twitter.com/MrAhmadAwais/
+ * Version: 1.2.0
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
